2751aa7cc86fb98048062f18debc6227120823d2
gachez <briangach98@gmail.com>
2021-01-18 21:27:37 +0300

    added loading states

--- services/libreconverter.js
+++ services/libreconverter.js
@@ -10,16 +10,18 @@ const fs = require('fs');
     // Read file
     const file = fs.readFileSync(file2convert);
     // Convert it to pdf format with undefined filter (see Libreoffice doc about filter)
-    alert('coverting...')
+    document.getElementsByClassName('overlay')[0].style.display = 'flex';
     libre.convert(file, extension2convert, undefined, (err, done) => {
         if (err) {
           console.log(`Error converting file: ${err}`);
+          document.getElementsByClassName('overlay')[0].style.display = 'none';
           alert('error converting the file.')
           return 0;
         }
         
         // Here in done you have pdf file which you can save or transfer in another stream
         console.log('success');
+        document.getElementsByClassName('overlay')[0].style.display = 'none';
         alert('File succesfully downloaded in the outputs folder');
         fs.writeFileSync(outputPath, done);
     });
--- services/mergeFiles.js
+++ services/mergeFiles.js
@@ -1,10 +1,12 @@
 const merge = require('easy-pdf-merge');
 
 const mergeFiles = (source_files, dest_file_path) => {
+    document.getElementsByClassName('overlay')[0].style.display = 'flex';
     merge(source_files, dest_file_path, function (err) {
         if (err) {
             return console.log(err)
         }
+        document.getElementsByClassName('overlay')[0].style.display = 'none';
         alert('Successfully merged')
         console.log('Success')
     });
